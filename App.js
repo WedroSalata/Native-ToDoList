57b9214926cfc0fe48621eb2bdc2e0600cea3f33
Evgen <evgen.kostin@appsdelivered.com>
2024-09-01 23:00:30 +0300

    update1

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
-import * as React from 'react';
+import react, { useState } from "react";
 import LoginPage from "./pages/loginPage";
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import ListPage from './pages/listPage';
+import { Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import ListPage from "./pages/listPage";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={LoginPage} />
-        <Stack.Screen name="List" component={ListPage} />
+      <Stack.Navigator>
+        {isSignedIn ? (
+          <Stack.Screen
+            name="List"
+            options={{
+              title: "ToDo List",
+              headerRight: () => (
+                <Text
+                  onPress={() => setIsSignedIn(false)}
+                  style={{ marginRight: 10, textDecorationLine: "underline" }}
+                >
+                  Log out
+                </Text>
+              ),
+            }}
+            component={ListPage}
+          />
+        ) : (
+          <Stack.Screen
+            name="Login"
+            component={LoginPage}
+            initialParams={{ setIsSignedIn: setIsSignedIn }}
+          />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
-  ); 
+  );
 }
--- pages/loginPage.tsx
+++ pages/loginPage.tsx
@@ -1,31 +1,33 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
 
-export default function LoginPage({navigation }) {
+export default function LoginPage({ route }) {
+  const { setIsSignedIn } = route.params;
+
   return (
     <View style={styles.container}>
-      <Text>Time to login into your To-Do!</Text>
-      <View>
-        <Text>Login</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="useless placeholder"
-          keyboardType="numeric"
-        />
-        <Text>Password</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="useless placeholder"
-          keyboardType="numeric"
-        />
+      <Text>ToDo List</Text>
+      <View style={styles.subcontainer}>
+        <Text>Login: </Text>
+        <TextInput style={styles.input} />
+        <Text>Password: </Text>
+        <TextInput style={styles.input} secureTextEntry />
       </View>
-      <Button
-        onPress={() => {navigation.navigate('List')}}
-        title="Sign in"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          setIsSignedIn(true);
+        }}
+      >
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -38,12 +40,32 @@ const styles = StyleSheet.create({
     borderStyle: "solid",
     borderRadius: 5,
     padding: "1%",
-    marginBottom: "1%",
+    marginVertical: 5,
   },
   container: {
     flex: 1,
+    width: 350,
+    margin: "auto",
+    padding: 10,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  subcontainer: {
+    width: "70%",
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: "#3F72AF",
+    borderRadius: 2,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.3,
+  },
+  buttonText: {
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    color: "#F9F7F7",
   },
 });
